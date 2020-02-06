e266ecd41b62720f2683f488cb10ba00b8e23af3
Nickolas Burr <nickolasburr@gmail.com>
2020-02-06 12:13:23 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,16 +1,15 @@
 # Project configuration
 project = 'Whiteboard'
-copyright = 'Copyright &copy; 2020 <a href="https://auroraextensions.com" target="_blank">Aurora Extensions</a>'
+copyright = '2020 Aurora Extensions'
 author = 'Aurora Extensions'
 release = '1.0.0'
 
 # General configuration
 extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
 
 # HTML configuration
-html_theme = 'nature'
+html_theme = 'bizstyle'
 html_static_path = ['_static']
 html_css_files = ['css/styles.css']
