b6fe96959b6546831664e72c01daffc1c6436782
Nickolas Burr <nickolasburr@gmail.com>
2020-02-06 14:03:04 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -11,6 +11,7 @@ exclude_patterns = []
 
 # HTML configuration
 html_theme = 'bizstyle'
+html_title = 'Whiteboard | Aurora Extensions'
 html_static_path = ['_static']
 html_css_files = ['css/styles.css']
 html_favicon = '_static/img/favicon.png'
